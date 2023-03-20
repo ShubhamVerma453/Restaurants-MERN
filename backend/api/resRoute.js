893e82c14c27cbc200a2892f604f5eba21a552fb
ShubhamVerma453 <sv4537373@gmail.com>
2023-03-20 19:15:30 +0530

    review controller completed now review DAO creating

--- backend/api/resRoute.js
+++ backend/api/resRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
 import resController from "./resController.js";
+import reviewCtrl from "./reviewController.js";
 
 const router = express.Router();
 
 router.route("/").get(resController.apiGetRestaurants);
+
+router.route("/review")
+    .post(reviewCtrl.apiAddReview)
+    .put(reviewCtrl.apiUpdateReview)
+    .delete(reviewCtrl.apiDeleteReview)
 
 export default router;
---
+++ backend/api/reviewController.js
@@ -0,0 +1,16 @@
+import RestaurantsDAO from "../dao/restaurantsDAO.js";
+
+export default class reviewCtrl {
+    static async apiAddReview(){
+
+    }
+
+    static async apiUpdateReview(){
+        
+    }
+
+    static async apiDeleteReview(){
+        
+    }
+
+}
